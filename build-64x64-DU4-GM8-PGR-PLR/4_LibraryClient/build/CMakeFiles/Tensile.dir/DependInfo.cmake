d169714082025d9113ab3d0bb4c88a40ed06ab99
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2019-09-18 05:57:39 +0000

    generate build-64x64-DU4-GM8-PGR-PLR code

---
+++ build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/Tensile/Kernels.cpp" "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/Tensile/Kernels.cpp.o"
+  "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/Tensile/SolutionHelper.cpp" "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/Tensile/SolutionHelper.cpp.o"
+  "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/Tensile/Solutions.cpp" "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/Tensile/Solutions.cpp.o"
+  "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/Tensile/Tensile.cpp" "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/Tensile/Tensile.cpp.o"
+  "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/Tensile/Tools.cpp" "/home/htony/Source/Tensile/1_MFMA/Tensile/build-64x64-DU4-GM8-PGR-PLR/4_LibraryClient/build/CMakeFiles/Tensile.dir/Tensile/Tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "Tensile_RUNTIME_LANGUAGE_HIP=1"
+  "Tensile_RUNTIME_LANGUAGE_OCL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Tensile"
+  "/opt/rocm/include"
+  "/opt/rocm/include/hcc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
