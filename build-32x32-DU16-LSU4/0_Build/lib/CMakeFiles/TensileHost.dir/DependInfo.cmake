a0fe8cecd47b1453acb01261b2a894445de3dfa6
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2019-10-01 06:06:08 +0000

    generate build-32x32-DU16-LSU4 code

---
+++ build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/AMDGPU.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/AMDGPU.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/ContractionProblem.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/ContractionProblem.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/ContractionSolution.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/ContractionSolution.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/DataTypes.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/DataTypes.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/Debug.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/Debug.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/EmbeddedLibrary.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/EmbeddedLibrary.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/KernelArguments.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/KernelArguments.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/Tensile.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/Tensile.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/TensorDescriptor.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/TensorDescriptor.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/TensorOps.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/TensorOps.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/Utils.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/Utils.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/hip/HipHardware.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/hip/HipHardware.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/hip/HipSolutionAdapter.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/hip/HipSolutionAdapter.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/llvm/Loading.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/llvm/Loading.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/source/llvm/YAML.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-32x32-DU16-LSU4/0_Build/lib/CMakeFiles/TensileHost.dir/source/llvm/YAML.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "TENSILE_DEFAULT_SERIALIZATION"
+  "TENSILE_STATIC_ONLY"
+  "TENSILE_USE_HIP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/include"
+  "/usr/lib/llvm-6.0/include"
+  "/opt/rocm/hip/include"
+  "/opt/rocm/hsa/include"
+  "/opt/rocm/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-32x32-DU16-LSU4/4_LibraryClient/build/Tensile/Tensile.cpp
@@ -0,0 +1,142 @@
+#include "Solutions.h"
+#include "Tensile.h"
+#include "TensileInternal.h"
+#include "SolutionMapper.h"
+
+/*******************************************************************************
+* Per-Problem Functions for Cijk_Ailk_Bljk_SB
+*******************************************************************************/
+// Problem type include the index assignments for free, summation, batch:
+static const ProblemType problemType_Cijk_Ailk_Bljk_SB( {0,1}, {3}, {2}, {0,3,2}, {3,1,2});
+
+// Master solution mapper is the entry point for problem->solution mapping
+// There is one master solution mapper per problem type
+// The master solution mapper contains pointers to the solution mappers for each device
+static MasterSolutionMapper<ProblemDims_Cijk_Ailk_Bljk_SB> masterSolutionMapper_Cijk_Ailk_Bljk_SB;
+ 
+/*******************************************************************************
+ * Per-Schedule Functions
+ *******************************************************************************/
+
+namespace { // Start schedule 'MI100'
+// solution table - function, name, assertion requirements
+static const SolutionInfo solutionTable_MI100_Cijk_Ailk_Bljk_SB[] = {
+  {(void*)Cijk_Ailk_Bljk_SB_MT32x32x16_SE_, "Cijk_Ailk_Bljk_SB_MT32x32x16_SE_", {1, 1, 1, 0, 1, 0, 0} } // 0
+};
+
+// table of exact problem dims and selected solutionIdx
+static const std::pair<const ProblemKey_Cijk_Ailk_Bljk_SB, int> embeddedExactTable_MI100_Cijk_Ailk_Bljk_SB[] = {
+ { {157, 177, 1, 53}, 0}  // 27 GFlop/s
+};
+
+// The solution master constructor here adds device to the master solution mapper
+// The entrypoint to find a solution for this problem is through the master solution master
+static SolutionMapper_Cijk_Ailk_Bljk_SB solutionMapper_MI100_Cijk_Ailk_Bljk_SB(
+  "MI100_Cijk_Ailk_Bljk_SB", // schedule+problem name
+  solutionTable_MI100_Cijk_Ailk_Bljk_SB, 1,
+  embeddedExactTable_MI100_Cijk_Ailk_Bljk_SB, 1,
+  &problemType_Cijk_Ailk_Bljk_SB);
+} // end anonymous namespace
+
+// problem dims -> solution logic
+SolutionMapper_Cijk_Ailk_Bljk_SB::SolutionRuntime *
+tensileGetSolutionPointer_Cijk_Ailk_Bljk_SB(
+    unsigned int strideD1J,
+    unsigned int strideD2K,
+    unsigned int strideC1J,
+    unsigned int strideC2K,
+    unsigned int strideA1L,
+    unsigned int strideA2K,
+    unsigned int strideB1J,
+    unsigned int strideB2K,
+    unsigned int sizeI,
+    unsigned int sizeJ,
+    unsigned int sizeK,
+    unsigned int sizeL) {
+
+  /* exact mappings */
+  ProblemDims_Cijk_Ailk_Bljk_SB pdims(strideD1J, strideD2K, strideC1J, strideC2K, strideA1L, strideA2K, strideB1J, strideB2K, sizeI, sizeJ, sizeK, sizeL);
+  auto solutionMapper = reinterpret_cast<SolutionMapper_Cijk_Ailk_Bljk_SB *> (masterSolutionMapper_Cijk_Ailk_Bljk_SB.mapper());
+  return solutionMapper->getSolutionWithFallback(pdims,&masterSolutionMapper_Cijk_Ailk_Bljk_SB);
+
+  return nullptr;
+
+}
+
+// get solution name for problem dims
+const char * tensileGetSolutionName_Cijk_Ailk_Bljk_SB(
+    unsigned int strideD1J,
+    unsigned int strideD2K,
+    unsigned int strideC1J,
+    unsigned int strideC2K,
+    unsigned int strideA1L,
+    unsigned int strideA2K,
+    unsigned int strideB1J,
+    unsigned int strideB2K,
+    unsigned int sizeI,
+    unsigned int sizeJ,
+    unsigned int sizeK,
+    unsigned int sizeL) {
+
+  /* exact mappings */
+  ProblemDims_Cijk_Ailk_Bljk_SB pdims(strideD1J, strideD2K, strideC1J, strideC2K, strideA1L, strideA2K, strideB1J, strideB2K, sizeI, sizeJ, sizeK, sizeL);
+  auto solutionMapper = reinterpret_cast<SolutionMapper_Cijk_Ailk_Bljk_SB *> (masterSolutionMapper_Cijk_Ailk_Bljk_SB.mapper());
+  return solutionMapper->getSolutionWithFallback(pdims,&masterSolutionMapper_Cijk_Ailk_Bljk_SB)->_info->_name;
+
+}
+/*******************************************************************************
+ * Per-ProblemType Functions
+ *******************************************************************************/
+// main call to solution; enqueues a kernel
+TensileStatus tensile_Cijk_Ailk_Bljk_SB(
+    float * dataD,
+    const float * dataC,
+    const float * dataA,
+    const float * dataB,
+    float alpha,
+    float beta,
+    unsigned int strideD1J,
+    unsigned int strideD2K,
+    unsigned int strideC1J,
+    unsigned int strideC2K,
+    unsigned int strideA1L,
+    unsigned int strideA2K,
+    unsigned int strideB1J,
+    unsigned int strideB2K,
+    unsigned int sizeI,
+    unsigned int sizeJ,
+    unsigned int sizeK,
+    unsigned int sizeL,
+    hipStream_t stream,
+    unsigned int numInputEvents,
+    hipEvent_t * inputEvents,
+    hipEvent_t * outputEvent) {
+    auto solution = tensileGetSolutionPointer_Cijk_Ailk_Bljk_SB(
+        strideD1J, 
+        strideD2K, 
+        strideC1J, 
+        strideC2K, 
+        strideA1L, 
+        strideA2K, 
+        strideB1J, 
+        strideB2K, 
+        sizeI, 
+        sizeJ, 
+        sizeK, 
+        sizeL);
+    if (solution) {
+      TensileSolutionPointer_Cijk_Ailk_Bljk_SB f = reinterpret_cast<TensileSolutionPointer_Cijk_Ailk_Bljk_SB> (solution->_info->_functionPtr);
+      auto solutionLock = &solution->_lock;
+      return f(solutionLock, dataD, dataC, dataA, dataB, alpha, beta, strideD1J, strideD2K, strideC1J, strideC2K, strideA1L, strideA2K, strideB1J, strideB2K, sizeI, sizeJ, sizeK, sizeL, stream, numInputEvents, inputEvents, outputEvent);
+    } else {
+      return tensileStatusFailure; // no solution found
+    }
+}
+
+/*******************************************************************************
+* Tensilze initializer
+*******************************************************************************/
+void tensileInitialize() {
+  masterSolutionMapper_Cijk_Ailk_Bljk_SB.initialize();
+  solutionMapper_MI100_Cijk_Ailk_Bljk_SB.initializeMappers({"Device 738c"},&masterSolutionMapper_Cijk_Ailk_Bljk_SB);
+}
