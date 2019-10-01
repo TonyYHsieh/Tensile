9ed5d317c2e45724bdb003cea31c8f28eb33c0d1
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2019-10-01 06:15:53 +0000

    generate build-64x64-DU4-GM8-PGR code

---
+++ build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/BenchmarkTimer.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/BenchmarkTimer.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/CSVStackFile.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/CSVStackFile.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/ClientProblemFactory.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/ClientProblemFactory.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/DataInitialization.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/DataInitialization.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/HardwareMonitor.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/HardwareMonitor.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/HardwareMonitorListener.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/HardwareMonitorListener.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/MetaRunListener.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/MetaRunListener.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/ProgressListener.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/ProgressListener.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/Reference.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/Reference.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/ReferenceValidator.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/ReferenceValidator.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/ResultFileReporter.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/ResultFileReporter.cpp.o"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/source/TimingEvents.cpp" "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/client/CMakeFiles/TensileClient.dir/source/TimingEvents.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "TENSILE_DEFAULT_SERIALIZATION"
+  "TENSILE_STATIC_ONLY"
+  "TENSILE_USE_HIP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/client/include"
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/Tensile/Source/lib/include"
+  "/opt/rocm/hip/include"
+  "/opt/rocm/hsa/include"
+  "/opt/rocm/include"
+  "/opt/rocm/rocm_smi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/htony/Source/Tensile/3_MFMA/Tensile/build-64x64-DU4-GM8-PGR/0_Build/lib/CMakeFiles/TensileHost.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
