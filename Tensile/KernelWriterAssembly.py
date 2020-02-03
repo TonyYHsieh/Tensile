fa257c2720dafe4ca6a5aece91a6df75c115d38a
Anthony Chang <ac.chang@outlook.com>
2020-02-03 11:02:23 +0800

    restore the vgpr pool, modified in openSumAtLeastUnroll(), in closeSumAtLeastUnroll() instead of in endSummation()
    or it would cause vgpr aliasing when one obtains temporary vgprs in the tail loop

--- Tensile/KernelWriterAssembly.py
+++ Tensile/KernelWriterAssembly.py
@@ -5301,8 +5301,6 @@ class KernelWriterAssembly(KernelWriter):
     kStr += self.comment1("endSummation: add vgpr %u...%u to pool" % \
             (self.startVgprValuA, self.lastVgprForReads))
 
-    if self.savedVgprPool != None:
-      self.vgprPool = self.savedVgprPool # restore vgprPool before alternate path
     self.vgprPool.add(self.startVgprValuA, \
         self.lastVgprForReads - self.startVgprValuA, "endSummation")
 
@@ -5670,6 +5668,9 @@ class KernelWriterAssembly(KernelWriter):
       else:
         label = self.getLabelNum("PrefetchGlobalLastIterEnd")
         kStr += "label_%04u:%s" % (label, self.endLine)
+    if self.savedVgprPool != None:
+      self.vgprPool = self.savedVgprPool # restore vgprPool before alternate path 
+      self.savedVgprPool = None
     return kStr
 
   ##############################################################################
