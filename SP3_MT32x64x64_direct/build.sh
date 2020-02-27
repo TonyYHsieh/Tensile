495a2c728d8bf4b8bcf77e1f93abe59de559e4e8
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2020-02-27 02:37:34 -0500

    add SP3 build tools

---
+++ SP3_MT32x64x64_direct/build.sh
@@ -0,0 +1,18 @@
+export LD_LIBRARY_PATH=../SP3_Tools/
+tool=../SP3_Tools/
+name=Cijk_Alik_Bljk_BBH_MT32x64x64_SE_K1
+client_kernel_path=../build_32x64/1_BenchmarkProblems/Cijk_Alik_Bljk_BBH_00/00_Final/source/assembly
+
+cp ${name}.sp3.c ${name}.sp3
+# /home/tonyh/Tools/SP3/sp3_asm.sh ${name}
+
+${tool}/mi100_sp3 ${name}.sp3 asic=MI9 type=cs -hex ${name}.hex
+${tool}/mi100_sp3 -hex ${name}.hex asic=MI9 type=cs  ${name}_out.sp3
+cat ${name}_out.sp3 | grep "// " | grep ": " | sed "s/.*: //" | sed "s/ \([0-9a-f]*\)/, 0x\1/" |sed "s/^/.long 0x/" > ${name}.inc
+
+cat ${name}.header.s > ${name}.s
+cat ${name}.inc >> ${name}.s
+${tool}/asm.sh ${name} -mcpu=gfx908
+cp ${name}.s  ${client_kernel_path}
+cp ${name}.o  ${client_kernel_path}
+cp ${name}.co ${client_kernel_path}
---
+++ SP3_MT32x64x64_direct/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name=Cijk_Alik_Bljk_BBH_MT32x64x64_SE_K1
+
+rm -rf ${name}.co
+rm -rf ${name}.hex
+rm -rf ${name}.inc
+rm -rf ${name}.o
+rm -rf ${name}_out.sp3
+rm -rf ${name}.s
+rm -rf ${name}.sp3
+
