13f42405eac2dfec03d9ee6829ecc94ef1d0fa39
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2020-03-03 22:26:51 -0500

    fix BBH_MT64x32x64 correctness issue
    when K = 128

--- SP3_MT64x32x64/Cijk_Alik_Bljk_BBH_MT64x32x64_SE_K1.sp3.c
+++ SP3_MT64x32x64/Cijk_Alik_Bljk_BBH_MT64x32x64_SE_K1.sp3.c
@@ -378,7 +378,7 @@ shader main
 
   s_lshr_b32       s[sgprLoopCounters+0], s[sgprSizesSum+0], 6 // s[sgprLoopCounters+0] = s[sgprSizesSum+0] / 64
   s_sub_u32        s[sgprLoopCounters+0], 0x0, s[sgprLoopCounters+0]
-  s_cmp_eq_u32     s[sgprLoopCounters+0], 0x0            // numIter0I == 0
+  s_cmp_ge_i32     s[sgprLoopCounters+0], -0x2            // numIter0I == 0
   s_cbranch_scc1   label_0006                           // Dont enter Loop
 
 /******************************************/
@@ -460,7 +460,7 @@ label_0005:
   s_mov_b32       m0, s[sgprLocalWriteAddrB+0]
   s_add_u32       s[sgprLoopCounters+0], s[sgprLoopCounters+0], 0x2        //inc CounterL
 
-  s_cmp_eq_i32    s[sgprLoopCounters+0], -0x2                    // CounterL=0x2
+  s_cmp_ge_i32    s[sgprLoopCounters+0], -0x2                    // CounterL=0x2
   s_cbranch_scc0  label_0005
 
 label_0006:
@@ -663,8 +663,8 @@ wave0_entry_start:
 
   s_lshr_b32      s[sgprLoopCounters+0], s[sgprSizesSum+0], 6 // s[sgprLoopCounters+0] = s[sgprSizesSum+0] / 64
   s_sub_u32       s[sgprLoopCounters+0], 0x0, s[sgprLoopCounters+0]
-  s_cmp_eq_u32    s[sgprLoopCounters+0], 0x0            // numIter0I == 0
-  s_cbranch_scc1  label_0004                           // Dont enter Loop
+  s_cmp_ge_i32    s[sgprLoopCounters+0], -0x2            // numIter0I == 0
+  s_cbranch_scc1  label_0002                             // Dont enter Loop
 
 /******************************************/
 /* Unrolled Loop(s) - Begin               */
@@ -720,7 +720,7 @@ label_0001:
 
   s_add_u32       s[sgprLoopCounters+0], s[sgprLoopCounters+0], 0x2        //inc CounterL
 
-  s_cmp_eq_i32    s[sgprLoopCounters+0], -0x2                              // CounterL=0x2
+  s_cmp_ge_i32    s[sgprLoopCounters+0], -0x2                              // CounterL=0x2
   s_cbranch_scc0  label_0001
 
 label_0002:
